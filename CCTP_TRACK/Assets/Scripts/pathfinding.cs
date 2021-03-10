befea2c94670a70c9efb906b807c418e0216569f
Joseph <josephgarrad2017@gmail.com>
2021-03-10 20:00:55 +0000

    doing something

--- CCTP_TRACK/Assets/Scripts/pathfinding.cs
+++ CCTP_TRACK/Assets/Scripts/pathfinding.cs
@@ -14,10 +14,10 @@ public class pathfinding : MonoBehaviour
     private Vector3 TrackRot;
     public int Retries = 0;
     public int Hill_Amount;
-
+    List<NodeScript > oldtrack = new List<NodeScript>();
     public int HillMovementCostToNeighb;
     private int FlatMovementCostToNeighb;
-
+    List<NodeScript> path = new List<NodeScript>();
     public bool Hilly_track;
     public bool Quickest_track;
     public bool flat_track;
@@ -25,6 +25,10 @@ public class pathfinding : MonoBehaviour
     {
         Grid = GetComponent<gride>(); //getting the gride script
         MS = MeshG.gameObject.GetComponent<MeshGenerator>();
+    }
+    private void Start()
+    {
+        
     }
     void Update()
     {
@@ -45,10 +49,12 @@ public class pathfinding : MonoBehaviour
             NodeScript currentNode = openSet[0];
             for(int i = 1; i < openSet.Count; i++)
             {
-                if( Retries == 0 && openSet[i].fcost < currentNode.fcost || openSet[i].fcost == currentNode.fcost && openSet[i].hCost < currentNode.hCost) // checking to see if the next node has a lower cost, if it does make it the current node
+                if(Retries == 0 && openSet[i].fcost < currentNode.fcost || openSet[i].fcost == currentNode.fcost && openSet[i].hCost < currentNode.hCost) // checking to see if the next node has a lower cost, if it does make it the current node
                 {
                     currentNode = openSet[i];
+                  
                 }
+               
             }
             openSet.Remove(currentNode); // when you move to the next node taske the cuuent node out of the open list so we cant go back to it 
             closeSet.Add(currentNode); // add to the closed list so we know its been tested;
@@ -56,13 +62,14 @@ public class pathfinding : MonoBehaviour
             if(currentNode == targetnode) // if we have hit out target then we are complete and leave the loop
             {
                 reTracePath(startnode, targetnode);
+                
                 return;
             }
             if (Quickest_track)
             {
                 foreach (NodeScript Neighbour in Grid.GetNeighbours(currentNode))
                 {
-                    if (!Neighbour.walkable || closeSet.Contains(Neighbour))
+                    if ( !Neighbour.walkable || closeSet.Contains(Neighbour))
                     {
                         continue;
                     }
@@ -83,8 +90,10 @@ public class pathfinding : MonoBehaviour
             }
             if (Hilly_track)
             {
+                
                 foreach (NodeScript Neighbour in Grid.GetNeighbours(currentNode))
                 {
+                    
                   
                     if (!Neighbour.walkable || closeSet.Contains(Neighbour))
                     {
@@ -116,48 +125,13 @@ public class pathfinding : MonoBehaviour
 
                 }
             }
-           /* if (flat_track)
-            {
-                foreach (NodeScript Neighbour in Grid.GetNeighbours(currentNode))
-                {
-
-                    if (!Neighbour.walkable || closeSet.Contains(Neighbour))
-                    {
-                        continue;
-                    }
-
-                    int newMovementCostToNeighb = currentNode.gCost + getDistance(currentNode, Neighbour);
-                    if (newMovementCostToNeighb < Neighbour.gCost || !openSet.Contains(Neighbour)) // checking to see if the nieghbour has a shorter path then the others or that it is not in the open list
-                    { // if the neighbour is shorter then set its cost to the distance it is away from the target node
-                        if (Neighbour.worldPos.y > currentNode.worldPos.y)
-                        {
-                            //currentNode.gCost -= 20;
-                            FlatMovementCostToNeighb = currentNode.gCost - 10 + getDistance(currentNode, Neighbour);
-                            Neighbour.gCost = FlatMovementCostToNeighb;
-                        }
-                        else
-                        {
-                            Neighbour.gCost = newMovementCostToNeighb;
-                        }
-
-                        Neighbour.hCost = getDistance(Neighbour, targetnode);
-                        Neighbour.parent = currentNode;
-
-                        if (!openSet.Contains(Neighbour))
-                        {
-                            openSet.Add(Neighbour);
-                        }
-                    }
-
-                }
-            }*/
         }
     }
     //somewhere above i need to create a function that chnages the cost of the neghbour is they have a higher y position
 
     void reTracePath(NodeScript startnode, NodeScript endnode)
     {
-        List<NodeScript> path = new List<NodeScript>();
+       
         //Debug.Log(path[path.Count]);
         NodeScript currentNode = endnode;
         while(currentNode != startnode)
